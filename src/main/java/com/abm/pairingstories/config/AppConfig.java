15ea7dcbf0c14e63cb78d98c56a440bbf899c615
AnandMuley <muley.anand66@gmail.com>
2018-06-02 10:24:44 +0530

    [AM]-Frontend setup using bootstrap framework

--- src/main/java/com/abm/pairingstories/config/AppConfig.java
+++ src/main/java/com/abm/pairingstories/config/AppConfig.java
@@ -3,15 +3,20 @@ package com.abm.pairingstories.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.abm.pairingstories")
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -22,4 +27,15 @@ public class AppConfig {
         return viewResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/external/**")
+                .addResourceLocations("/node_modules/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS)
+                        .cachePublic());
+        registry.addResourceHandler("/internal/**")
+                .addResourceLocations("/static/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.MILLISECONDS)
+                        .cachePublic());
+    }
 }
